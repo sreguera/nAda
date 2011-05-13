aa67fbc2f49ef252c7b9b5decce394ee305a6fa9
Sebastián <sreguera@gmail.com>
2011-05-13 22:48:04 +0200

    Beginning of the parser.

---
+++ src/nada.pl
@@ -0,0 +1,54 @@
+/*
+  Part of nAda.
+  Copyright (c) 2011 Jose Sebastian Reguera Candal.
+*/
+:- module(nada, []).
+
+parse(Tokens, AST) :-
+        phrase(subprogram_body(AST), Tokens).
+
+%-----------------------------------------------------------------------
+% Section 2: Lexical Elements
+%-----------------------------------------------------------------------
+% 2.3 Identifiers
+
+identifier(X) -->
+        [X], {atom(X)}. % TODO
+
+%-----------------------------------------------------------------------
+% Section 3: Declarations and Types
+%-----------------------------------------------------------------------
+% 3.1 Declarations
+
+defining_identifier(Id) -->
+        identifier(Id).
+
+%-----------------------------------------------------------------------
+% Section 6: Subprograms
+%-----------------------------------------------------------------------
+% 6.1 Subprogram Declarations
+
+subprogram_specification(Proc_Spec) -->
+        procedure_specification(Proc_Spec).
+
+procedure_specification(proc_spec(Name)) -->
+        ['procedure'], defining_program_unit_name(Name).
+
+defining_program_unit_name(Name) -->
+        defining_identifier(Name).
+
+%-----------------------------------------------------------------------
+% 6.3 Subprogram Bodies
+
+subprogram_body(proc_body(Name)) -->
+        subprogram_specification(proc_spec(Name)), ['is'],
+        ['begin'],
+        ['end'], identifier(Name), [';'].        
+
+%-----------------------------------------------------------------------
+
+test1 :-
+        phrase(subprogram_body(proc_body('X')),
+               ['procedure', 'X', 'is',
+                'begin',
+                'end', 'X', ';']).
