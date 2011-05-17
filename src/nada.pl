91950b0b865838129652027fd8628e3f8a1085dd
Sebastián <sreguera@gmail.com>
2011-05-17 22:48:37 +0200

    Parse variable declarations.

--- src/nada.pl
+++ src/nada.pl
@@ -13,15 +13,64 @@ parse(Tokens, AST) :-
 % 2.3 Identifiers
 
 identifier(X) -->
-        [X], {atom(X)}. % TODO
+        [id(X)]. % TODO
 
 %-----------------------------------------------------------------------
 % Section 3: Declarations and Types
 %-----------------------------------------------------------------------
 % 3.1 Declarations
 
+basic_declaration(Decl) -->
+        object_declaration(Decl).
+
 defining_identifier(Id) -->
         identifier(Id).
+
+
+%-----------------------------------------------------------------------
+% 3.3 Objects and Named Numbers
+
+% 3.2.2 Subtype Declarations
+
+subtype_indication(Subtype) -->
+        subtype_mark(Subtype).
+
+subtype_mark(Subtype) -->
+        subtype_name(Subtype).
+
+subtype_name(Subtype) -->
+        identifier(Subtype). % TODO
+
+% 3.3.1 Object Declarations
+
+object_declaration(decl(Ids, Subtype)) -->
+        defining_identifier_list(Ids), [':'],
+        subtype_indication(Subtype), [';'].
+
+defining_identifier_list([Id|Ids]) -->
+        defining_identifier(Id),
+        defining_identifier_list_rest(Ids).
+
+defining_identifier_list_rest([Id|Ids]) -->
+        [','], !, defining_identifier(Id), % TODO ! to avoid choice point, ok?
+        defining_identifier_list_rest(Ids).
+defining_identifier_list_rest([]) -->
+        [].
+
+%-----------------------------------------------------------------------
+% 3.11 Declarative Parts
+
+declarative_part([Decl|Decls]) -->
+        declarative_item(Decl), !, % TODO ! to avoid choice point, ok?
+        declarative_part(Decls).
+declarative_part([]) -->
+        [].
+
+declarative_item(Decl) -->
+        basic_declarative_item(Decl).
+
+basic_declarative_item(Decl) -->
+        basic_declaration(Decl).
 
 %-----------------------------------------------------------------------
 % Section 6: Subprograms
@@ -40,8 +89,9 @@ defining_program_unit_name(Name) -->
 %-----------------------------------------------------------------------
 % 6.3 Subprogram Bodies
 
-subprogram_body(proc_body(Name)) -->
+subprogram_body(proc_body(Name, Decls)) -->
         subprogram_specification(proc_spec(Name)), ['is'],
+        declarative_part(Decls),
         ['begin'],
         ['end'], identifier(Name), [';'].        
 
@@ -50,9 +100,19 @@ subprogram_body(proc_body(Name)) -->
 :- begin_tests(nada).
 
 test(empty_procedure) :-
-        phrase(subprogram_body(proc_body('X')),
-               ['procedure', 'X', 'is',
+        phrase(subprogram_body(proc_body('X', [])),
+               ['procedure', id('X'), 'is',
                 'begin',
-                'end', 'X', ';']).
+                'end', id('X'), ';']).
+
+test(procedure_with_declarations) :-
+        phrase(subprogram_body(proc_body('X',
+                                         [decl(['a', 'b'], 'byte'),
+                                          decl(['c'], 'word')])),
+               ['procedure', id('X'), 'is',
+                   id('a'), ',', id('b'), ':', id('byte'), ';',
+                   id('c'), ':', id('word'), ';',
+                'begin',
+                'end', id('X'), ';']).
 
 end_tests(nada).
