f2b153dbd3b2d030f884f2883f7b3e58bc520cfe
Sebastián <sreguera@gmail.com>
2011-06-18 18:00:57 +0200

    Added check for duplicated declarations.

--- src/sem.pl
+++ src/sem.pl
@@ -5,24 +5,27 @@
 :- module(sem, []).
 
 transform(AST, ASG) :-
-        pass_subprogram_body(AST, ASG).
+        process_subprogram_body(AST, ASG).
 
-pass_subprogram_body(proc_body(Name, Decls),
-                     proc_body(Name, SemDecls)) :-
-        pass_declarations(Decls, SemDecls).
+process_subprogram_body(proc_body(Name, Decls),
+                        proc_body(Name, Symbols)) :-
+        process_declarations(Decls, Symbols).
 
-% pass_declarations(+Declarations, -SemanticDeclarations)
-pass_declarations([decl(Ns, Type)|Ds], SDs) :-
-        pass_declaration(Ns, Type, HSDs),
-        pass_declarations(Ds, TSDs),
-        append(HSDs, TSDs, SDs).
-pass_declarations([], []).
+%% process_declarations(+Declarations, -Symbols)
+process_declarations(Decls, Symbols) :-
+        process_declarations(Decls, [], Symbols).
 
-pass_declaration([N|Ns], Type, [decl(N, Type)|SDs]) :-
-        pass_declaration(Ns, Type, SDs).
-pass_declaration([], _, []).
+process_declarations([], S, S).
+process_declarations([decl(Ns, Type)|Ds], S0, S) :-
+        process_declaration(Ns, Type, S0, S1),
+        process_declarations(Ds, S1, S).
 
-        
+process_declaration([], _, S, S).
+process_declaration([N|Ns], Type, S0, S) :-
+        (  memberchk(symbol(N, _), S0)
+        -> throw(duplicated_declaration)
+        ;  process_declaration(Ns, Type, [symbol(N, Type)|S0], S)
+        ).
 
 %-----------------------------------------------------------------------
 
@@ -33,8 +36,21 @@ test(procedure_with_declarations) :-
                                        [decl(['a', 'b'], 'byte'),
                                         decl(['c'], 'word')]),
                              proc_body('X',
-                                       [decl('a', 'byte'),
-                                        decl('b', 'byte'),
-                                        decl('c', 'word')])).
+                                       [symbol('c', 'word'),
+                                        symbol('b', 'byte'),
+                                        symbol('a', 'byte')])).
+
+test(procedure_with_duplicated_declarations,
+     [throws(duplicated_declaration)]) :-
+        pass_subprogram_body(proc_body('X',
+                                       [decl(['a'], 'byte'),
+                                        decl(['a'], 'word')]),
+                             _).
+
+test(procedure_with_duplicated_declarations2,
+     [throws(duplicated_declaration)]) :-
+        pass_subprogram_body(proc_body('X',
+                                       [decl(['a', 'a'], 'byte')]),
+                             _).
 
 :- end_tests(sem).
