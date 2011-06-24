60ae974121ce6109904e2b5c176915391ccf8d88
Sebastián <sreguera@gmail.com>
2011-06-24 22:39:20 +0200

    Used difference lists/DCGs in the assembler.

--- src/asm.pl
+++ src/asm.pl
@@ -4,26 +4,28 @@
 */
 :- module(asm, []).
 
-assemble(Insts, Codes) :-
-        asm(Insts, Codes0),
-        append(Codes0, Codes).
+assemble(Insts, Bytes) :-
+        asm(Insts, Bytes, []).
 
-asm([], []).
-asm([I|Is], [C|Cs]) :-
-        asm_inst(I, C),
-        asm(Is, Cs).
+asm([]) -->
+        [].
+asm([I|Is]) -->
+        asm_inst(I),
+        asm(Is).
 
-asm_inst(add(ix, sp),     [0xDD, 0x39]).
-asm_inst(ret,             [0xC9]).
-asm_inst(ld(A1, A2), Bytes) :-
-        asm_ld(A1, A2, Bytes).
-asm_inst(pop(ix),         [0xDD, 0xE1]).
-asm_inst(push(ix),        [0xDD, 0xE5]).
+asm_inst(add(ix, sp))   -->     [0xDD, 0x39].
+asm_inst(ret)           -->     [0xC9].
+asm_inst(ld(A1, A2))    -->     asm_ld(A1, A2).
+asm_inst(pop(ix))       -->     [0xDD, 0xE1].
+asm_inst(push(ix))      -->     [0xDD, 0xE5].
 
-asm_ld(ix, NN, [0xDD, 0x21, N0, N1]) :-
-        N0 is NN /\ 0xFF,
-        N1 is (NN >> 8) /\ 0xFF.
-asm_ld(sp, ix, [0xDD, 0xF9]).
+asm_ld(ix, NN) -->
+        [0xDD, 0x21, N0, N1],
+        {   N0 is NN /\ 0xFF,
+            N1 is (NN >> 8) /\ 0xFF
+        }.
+asm_ld(sp, ix) -->
+        [0xDD, 0xF9].
 
 
 %-----------------------------------------------------------------------
